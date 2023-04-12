9cd2a1e37851f72b5b1c2f3c7ac760a35ec8fdfa
Bret Wagner <tubaking182@gmail.com>
2023-04-12 14:49:25 -0500

    Added some documentation to functions

--- dsmreftab.py
+++ dsmreftab.py
@@ -52,6 +52,13 @@ def create_user_reftab(userinfo):
         return userinfo
 
 def search_users(username):
+    """
+    Seach Reftab for user account by username
+    username    str     this should be the username to search
+
+    Return
+    response    list    an array of user objects that match the search
+    """
     try:
         response = client.get('loanees', query=username, limit=1)
     except:
@@ -64,6 +71,14 @@ def search_users(username):
         return response
 
 def get_loans(userid=None, loan_id=None):
+    """ 
+    Get individual (loan_id) or all loans (userid)
+    userid      int/str     loanee ID in Reftab
+    loan_id     int/str     loan ID in Reftab
+
+    Returns
+    loan(s)     if using userid, you may receive multiple results
+    """
     if (userid):
         try:
             response = client.get('loans', loan_uid=str(userid))
@@ -89,11 +104,26 @@ def get_loans(userid=None, loan_id=None):
         sys.exit("Unable to retrieve loans")
 
 def get_asset(asset_id):
+    """
+    Get the asset object from the Reftab API
+    asset_id    int/str     Asset ID in Reftab
+
+    Return
+    response    dict        Asset object from Reftab
+    """
     response = client.get('assets', id=str(asset_id))
 
     return response
 
-def update_asset(asset, status):
+def update_asset_status(asset, status):
+    """
+    Updates the status label of a given asset
+    asset       dict    asset object from Reftab
+    status      str     this should be one of the available labels (more to be added)
+
+    Return
+    response    dict    asset object (or error)
+    """
     if status == "term":
         asset['status'] = {
                             'color': '#ddec09',
@@ -106,15 +136,32 @@ def update_asset(asset, status):
     return response
 
 def terminate_user(username):
-    # print("searching users")
+    """
+    Used when terminating an employee, updates computer due date to today
+    and changes the status label for the computer.
+    username    str     username of the employee (ex: jsmith)
+
+    Return (None)
+    response    dict    this is the response from the API when updating the asset
+                        (not the loan due date, but the due date will be shown on success)
+    """
     user = search_users(username)[0]
-    userid = user['lnid']
-    # print("getting loans for user")
+
+    if len(user) > 1:
+        sys.exit("Multiple users match that search, please provide the unique username")
+    elif len(user) == 0:
+        sys.exit("No users matched the search, please correct the username")
+
+
+    try:
+        userid = user['lnid']
+    except KeyError:
+        sys.exit("No matching username, check the spelling")
+
+    
     loans = get_loans(userid=userid)
 
     for loan in loans:
-        # os.system("clear")
-        # pprint(loan)
         category = loan['categoryName']
         if "MacBook" in category or "iMac" in category:
             loan_id = loan['lid']
@@ -122,25 +169,30 @@ def terminate_user(username):
         if "MacBook" in category:
             asset_id = loan['aid']
 
+        if "iMac" in category:
+            status = "in"
+        else:
+            status = "out"
+
     # Update device due date to TODAY at 5PM
     due_date = datetime.today().strftime('%Y-%m-%d') + "T22:00:00Z"
     # Update device status to "Needs shipped home"
     
     body = {
-            'status': 'out',
+            'status': status,
             'notes': 'Employee terminated',
             'due': str(due_date)
             }
 
-    # pprint(body)
     # update the loan status
     client.put('loans', id=str(loan_id), body=body)
 
     # update the asset status label
     asset = get_asset(asset_id)
-    response = update_asset(asset, "term")
+    response = update_asset_status(asset, "term")
 
     print("User's device has been updated in Reftab")
 
-    return response
+    # Uncomment for debugging purposes or to see the asset response
+    # return response
 
